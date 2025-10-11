698cd194dc894c673f9ee3839aafa2aef18dca31
oogway7h <140742249+oogway7h@users.noreply.github.com>
2025-10-11 14:58:50 -0400

    cerrar sesion

--- apps/cuentas/views.py
+++ apps/cuentas/views.py
@@ -299,7 +299,28 @@ class UsuarioViewSet(MultiTenantMixin, viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
     def logout(self, request):
-        return Response({"message": "Cierre de sesión exitoso"}, status=status.HTTP_200_OK)
+        #eliminar token
+        user = request.user
+        try:
+        
+            Token.objects.filter(user=user).delete()
+            
+            #registrar en bitacora
+            actor = get_actor_usuario_from_request(request)
+            log_action(
+                request=request,
+                accion=f"Cierre de sesión del usuario {user.username}",
+                objeto=f"Usuario: {user.username}",
+                usuario=actor
+            )
+            
+            return Response({"message": "Cierre de sesión exitoso"}, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            return Response(
+                {"error": f"Ocurrió un error al cerrar sesión: {str(e)}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 class BitacoraListAPIView(MultiTenantMixin, generics.ListAPIView):
     permission_classes = [IsAuthenticated]
